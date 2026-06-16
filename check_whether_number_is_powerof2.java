454bb7b1a811394b91338a6cd397bf55e474015e
jeeva19340 <varameshsvks_bcs27@mepcoeng.ac.in>
2026-06-16 09:14:25 +0530

    Implement method to check if number is power of two

---
+++ check_whether_number_is_powerof2.java
@@ -0,0 +1,12 @@
+public class PowerOfTwo {
+
+    static boolean isPowerOfTwo(int n) {
+        return n > 0 && ((n & (n - 1)) == 0);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(isPowerOfTwo(16));
+        System.out.println(isPowerOfTwo(18));
+    }
+}
